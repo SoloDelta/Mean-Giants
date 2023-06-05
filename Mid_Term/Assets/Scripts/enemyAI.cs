eff0a581eb4e78ff9b64041f7049ed8cec8943d1
SlumbrJack <jakewfnc@gmail.com>
2023-06-04 22:13:34 -0400

    Enemy Patrol Path done

--- Mid_Term/Assets/Scripts/enemyAI.cs
+++ Mid_Term/Assets/Scripts/enemyAI.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 //Project: Mean_Giants Midterm
 public class enemyAI : MonoBehaviour, IDamage
 {
     [Header("-----Components-----")]
     [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform headPosition;
+
 
     [Header("-----Enemy Stats-----")]
     [SerializeField] int HP;
+    [SerializeField] float speed;
+    [SerializeField] int playerFaceSpeed;
+    [SerializeField] int viewConeAngle;
+    [SerializeField] Vector3 startPosition;
+    [SerializeField] Vector3 endPosition;
+    [SerializeField] bool atStart;
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +31,16 @@ public class enemyAI : MonoBehaviour, IDamage
     // Update is called once per frame
     void Update()
     {
-        
+        moveOnPatrol();
     }
+
     public void TakeDamage(int dmg)
     {
         HP -= dmg;
         StartCoroutine(flashColor());
         if(HP <= 0)
         {
-            //decrement enemies remaining in GM
+            //TODO: decrement enemies remaining in GM
             Destroy(gameObject);
         }
     }
@@ -39,4 +51,30 @@ public class enemyAI : MonoBehaviour, IDamage
         yield return new WaitForSeconds(0.1f);
         model.material.color = Color.white;
     }
+
+    void moveOnPatrol()
+    {
+        //NOTE: This code will do for now, but setting the y position will cause issues on multi storied scenes.
+        if(atStart)
+        {
+            agent.SetDestination(endPosition);
+            if(new Vector3(transform.position.x, startPosition.y, transform.position.z) == endPosition)
+            {
+                atStart = !atStart;
+                Debug.Log("Swap");
+            }
+        }
+        else
+        {
+            agent.SetDestination(startPosition);
+            if (new Vector3(transform.position.x, startPosition.y, transform.position.z) == startPosition)
+            {
+                atStart = !atStart;
+                Debug.Log("Swap");
+            }
+        }
+        
+
+    }
+    //TODO Add code for chasing player once game manager is  set up
 }
