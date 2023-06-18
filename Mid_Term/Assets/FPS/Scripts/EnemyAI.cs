e8a235e01d2ba397049c834867dfa230d513af96
SlumbrJack <jakewfnc@gmail.com>
2023-06-18 11:11:28 -0400

    Commented code

--- Mid_Term/Assets/FPS/Scripts/EnemyAI.cs
+++ Mid_Term/Assets/FPS/Scripts/EnemyAI.cs
@@ -25,65 +25,62 @@ namespace FPS
     public class EnemyAI : MonoBehaviour, IDamage
     {
         [Header("-----Components-----")]
-        [SerializeField] private Renderer model;
-        [SerializeField] private NavMeshAgent agent;
-        [SerializeField] private Animator anim;
-        [SerializeField] private Transform headPosition;
-        [SerializeField] private Transform shootPosition;
-        [SerializeField] private GameObject lineRenderer;
+        [SerializeField] private Renderer model; //the model of the enemy
+        [SerializeField] private NavMeshAgent agent; //the enemy's navmesh
+        [SerializeField] private Animator anim; //the enemy's animator
+        [SerializeField] private Transform headPosition; //the position of the players head. used for more direct raycasts
+        [SerializeField] private Transform shootPosition; //the position of where the bullet comes out
+        [SerializeField] private GameObject lineRenderer; //line renderer used for laser sight
 
         [Header("----- UIComponents-----")]
-        [SerializeField] private GameObject enemyUIParent;
-        [SerializeField] public GameObject wholeHealthBar;
-        [SerializeField] private Transform HPBar;
-        [SerializeField] private GameObject spottingUI;
-        [SerializeField] private GameObject spottedUI;
-        [SerializeField] private Transform qmarkTransform;
+        [SerializeField] private GameObject enemyUIParent; //the entirety of enemy UI. Healthbar and detection.
+        [SerializeField] public GameObject wholeHealthBar; //the healthbar parent
+        [SerializeField] private Transform HPBar; //the healthbar itself
+        [SerializeField] private GameObject spottingUI; // question mark UI
+        [SerializeField] private GameObject spottedUI; //exclamation point UI
+        [SerializeField] private Transform qmarkTransform; //the red question mark that gets scaled with being spotted
 
         [Header("-----Enemy Stats-----")]
-        [SerializeField] private int HP;
-        [SerializeField] private float speed;
-        [SerializeField] private int playerFaceSpeed;
-        [SerializeField] private int viewConeAngle;
-        [SerializeField] private bool atStart;
+        [SerializeField] private int HP; //the health of the enemy
+        [SerializeField] private float speed; //speed at which the enemy moves
+        [SerializeField] private int playerFaceSpeed; //the speed at which the enemy rotates to the player when stopped
+        [SerializeField] private int viewConeAngle; //the angle from which the enemy can see the player (calculated from middle)
+        [SerializeField] private bool atStart; //a visual bool just to show if the AI is where it started.
 
         [Header("-----Roaming-----")]
-        [SerializeField, Range(1, 10)] private float roamTimer;
-        [SerializeField, Range(10, 100)] private int roamDist;
+        [SerializeField, Range(1, 10)] private float roamTimer; //how long the enemy waits before roaming
+        [SerializeField, Range(10, 100)] private int roamDist; //how far away the enemy will roam
 
         [Header("-----Pathfinding-----")]
-        [SerializeField] private List<Vector3> patrolSpots = new List<Vector3>();
-        [SerializeField] private List<int> patrolRotations = new List<int>();
+        [SerializeField] private List<Vector3> patrolSpots = new List<Vector3>(); //the locations the enemy will follow when patroling
+        [SerializeField] private List<int> patrolRotations = new List<int>(); //the rotations the enemy will face too at once reaching a patrol spot
 
         [Header("-----Enemy Stats-----")]
-        [SerializeField] private float shootRate;
-        [SerializeField] private GameObject bullet;
-        [SerializeField] private float burstRate;
-        [SerializeField] private bool isBurstShot;
-        private int numOfPatrolSpots;
-        private Vector3 playerDirection;
-        private bool playerInRange;
-        private float angleToPlayer;
-        private bool isShooting;
-        private bool seesPlayer;
-        private bool destinationChosen;
-        private Vector3 startingPos;
-        private float stoppingDistanceOriginal;
-        private bool isPatrolling;
-        private int currentPointIndex;
-        private GameObject player;
-        private int enemyHPOriginal;
-        private float percentSpotted;
-        private bool spotted = false;
-        private Coroutine losingPlayerCopy;
-        private Coroutine lookAroundCopy;
-        private bool sawPlayerTemp = false;
-        private float timeCount = 0.0f;
+        [SerializeField] private float shootRate; //how fast the enemy shoots
+        [SerializeField] private GameObject bullet; //the bullet the enemy shoots
+        [SerializeField] private float burstRate; //how fast of a burst the enemy shoots
+        [SerializeField] private bool isBurstShot; //if the enemy shoots regular or in burst
+        private int numOfPatrolSpots; //the number of spots the enemy patrols to
+        private Vector3 playerDirection; //the direction from the enemy to the player
+        private bool playerInRange; //is the player within range of the enemy
+        private float angleToPlayer; //the angle to the player
+        private bool isShooting; //is the enemy actively shooting
+        private bool seesPlayer; //does the enemy see the player
+        private bool destinationChosen; //bool used in roam to show if the enemy has found a spot to roam to
+        private Vector3 startingPos; //where the enemy was before roam started
+        private float stoppingDistanceOriginal; //the original stopping distance of the enemy
+        private bool isPatrolling; //if the enemy patrols or roams
+        private int currentPointIndex; //the index of the last patrol spot the enemy was at
+        private int enemyHPOriginal; //starting hp of the enemy
+        private float percentSpotted; //float repressenting how much the player has been spotted
+        private bool spotted = false; //has the enemy spotted the player
+        private Coroutine losingPlayerCopy; //a variable to store the last coroutine of "losingPlayer"
+        private bool sawPlayerTemp = false; //did a raycast connect but not long enough for the player to be spotted
+        private float timeCount = 0.0f; //time variable for slerping after a patrol
         private void Start()
         {
-            GameManager.instance.UpdateObjective(1);
-            //player = GameObject.FindGameObjectWithTag("Player");
-            player = GameManager.instance.player;
+            GameManager.instance.UpdateObjective(1); 
+            //initializing variables
             startingPos = transform.position;
             agent.speed = speed;
             enemyHPOriginal = HP;
@@ -109,9 +106,6 @@ namespace FPS
           
         }
 
-        /**----------------------------------------------------------------
-         * @brief MonoBehaviour override.
-         */
         private void Update()
         {
             
@@ -119,7 +113,7 @@ namespace FPS
             {
                 anim.SetFloat("Enemy Speed", agent.velocity.normalized.magnitude);
                 rotateUI();
-                if(true) //removed playerinrange
+                if(true) //removed playerinrange. Needs fix
                 {
                     seesPlayer = canSeePlayer();
                     enemyNav();
@@ -130,10 +124,8 @@ namespace FPS
             
         }
 
-        /**----------------------------------------------------------------
-         * @brief
-         */
-        private void enemyNav()
+
+        private void enemyNav() //Mega Function to determine how the enemy will navigate. 
         {
             if (!spotted) //if the player has not been spotted update percent spotted
             {
@@ -147,9 +139,9 @@ namespace FPS
                     agent.isStopped = true;
                     sawPlayerTemp = true;
                 }
-                else
+                else //if the player has been spotted
                 {
-                    if (losingPlayerCopy != null)
+                    if (losingPlayerCopy != null) //if the enemy has started losing the player, stop losing the player.
                     {
                         StopCoroutine(losingPlayerCopy);
                         losingPlayerCopy = null;
@@ -159,25 +151,25 @@ namespace FPS
                     agent.isStopped = false;
                     agent.stoppingDistance = stoppingDistanceOriginal;
                     agent.SetDestination(GameManager.instance.player.transform.position);
-                    if (agent.remainingDistance <= agent.stoppingDistance)
+                    if (agent.remainingDistance <= agent.stoppingDistance) //ensures the enemy still tracks the player with rotation after reaching its stopping point
                     {
                         facePlayer();
                     }
-                    if (!isShooting)
+                    if (!isShooting) //shoot
                     {
                         StartCoroutine(shoot());
                     }
                 }
             }
-            else
+            else //if the enemy does not see the player
             {
                 
-                if (spotted)
+                if (spotted) //if the player has been spotted, go to the players last seen location and look around. 
                 {
                     agent.isStopped = false;
                     agent.stoppingDistance = 0;
 
-                    if (agent.destination.x == transform.position.x && agent.destination.z == transform.position.z)
+                    if (agent.destination.x == transform.position.x && agent.destination.z == transform.position.z) 
                     {
 
                         if (losingPlayerCopy == null)
@@ -190,7 +182,7 @@ namespace FPS
                         StartCoroutine(roam());
                     }
                 }
-                else
+                else //if the player hasnt been spotted, go back to original action, patroling or roaming
                 {
                     if(sawPlayerTemp)
                     {
@@ -218,10 +210,7 @@ namespace FPS
             }
         }
 
-        /**----------------------------------------------------------------
-         * @brief
-         */
-        private bool canSeePlayer()
+        private bool canSeePlayer() //raycasts to the player to see if the player can be seen. Need to update player in range
         {
             if (playerInRange)
             {
@@ -248,18 +237,13 @@ namespace FPS
             return false;
         }
 
-        /**----------------------------------------------------------------
-         * @brief
-         */
-        private void spotting(float _deltaTime)
+
+        private void spotting(float _deltaTime) //starts detecting the player over time
         {
             if (seesPlayer && percentSpotted < 1)
             {
                 percentSpotted += 0.5f * _deltaTime;
                 qmarkTransform.localScale = new Vector3((4 * percentSpotted), qmarkTransform.localScale.y, qmarkTransform.localScale.z);
-                //Debug.Log(qmarkTransform.localScale.x);
-                //Debug.Log(percentSpotted);
-
             }
             else if (!seesPlayer && percentSpotted > 0)
             {
@@ -278,10 +262,6 @@ namespace FPS
                 spottingUI.SetActive(false);
             }
         }
-
-        /**----------------------------------------------------------------
-         * @brief
-         */
         private void patrolCirculation() //controls the logic for pathing. if the enemy has a patrol route and doesnt see the player, he patrols. if he doesnt have a patrol he roams.
         {
             if (isPatrolling && !seesPlayer)
@@ -314,12 +294,10 @@ namespace FPS
             else { StartCoroutine(roam()); }
         }
         
-        /**----------------------------------------------------------------
-         * @brief
-         */
-        private void rotateUI()
+
+        private void rotateUI() //rotates the enemy's UI towards the player
         {
-            if (wholeHealthBar.activeInHierarchy || spottedUI.activeInHierarchy || spottingUI.activeInHierarchy) //if the health bar is active, set its x rotation to that of the camera and set the y rotation to that of the player
+            if (wholeHealthBar.activeInHierarchy || spottedUI.activeInHierarchy || spottingUI.activeInHierarchy) //if ui is active, set its x rotation to that of the camera and set the y rotation to that of the player
             {
                 Vector3 eulerAngleRots = new Vector3(Camera.main.transform.rotation.eulerAngles.x, GameManager.instance.player.transform.rotation.eulerAngles.y, 0.0f);
                 Quaternion rotation = Quaternion.Euler(eulerAngleRots);
@@ -327,10 +305,8 @@ namespace FPS
             }
         }
 
-        /**----------------------------------------------------------------
-         * @brief
-         */
-        private void OnTriggerEnter(Collider other)
+
+        private void OnTriggerEnter(Collider other) //sets player in range if in range
         {
             if (other.CompareTag("Player"))
             {
@@ -341,7 +317,7 @@ namespace FPS
         /**----------------------------------------------------------------
          * @brief
          */
-        private void OnTriggerExit(Collider other)
+        private void OnTriggerExit(Collider other) //sets player in range if in not range
         {
             if (other.CompareTag("Player"))
             {
@@ -349,15 +325,13 @@ namespace FPS
             }
         }
 
-        /**----------------------------------------------------------------
-         * @brief
-         */
-        public void TakeDamage(int dmg)
+
+        public void TakeDamage(int dmg) //logic for taking damage. 
         {
             HP -= dmg;
             
             updateEnemyUI();
-            if (HP <= 0)
+            if (HP <= 0) //if the enemy is dead, turns of lasersight, stops all active coroutines, stops animations, and turns off collision.
             {
                 if (lineRenderer != null)
                 {
@@ -374,7 +348,7 @@ namespace FPS
                 GetComponent<CapsuleCollider>().enabled = false;
                 
             }
-            else
+            else //starts attacking the player and instantly spots them
             {
                 agent.SetDestination(GameManager.instance.player.transform.position);
                 if(!spotted)
@@ -387,27 +361,21 @@ namespace FPS
             }
         }
 
-        /**----------------------------------------------------------------
-         * @brief
-         */
-        private void updateEnemyUI()
+
+        private void updateEnemyUI() // Updates enemyHP bar
         {
             HPBar.transform.localScale = new Vector3((float)HP / enemyHPOriginal, HPBar.localScale.y, HPBar.localScale.y);
         }
 
-        /**----------------------------------------------------------------
-         * @brief
-         */
+
         private void facePlayer() //faces the player. Called when enemy is at stopping distance
         {
             Quaternion rot = Quaternion.LookRotation(new Vector3(playerDirection.x, 0, playerDirection.z));
             transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * playerFaceSpeed);
         }
         
-        /**----------------------------------------------------------------
-         * @brief
-         */
-        public void createBullet()
+
+        public void createBullet() //spawns the bullet at a position. needs work
         {
             Instantiate(bullet, shootPosition.position, transform.rotation); //no shooting at the player, need to be changed
         }
@@ -415,7 +383,7 @@ namespace FPS
         /**----------------------------------------------------------------
          * @brief Coroutine to make enemy shoot at the player.
          */
-        private IEnumerator shoot()
+        private IEnumerator shoot() //coroutine to control shooting logic. Shoots a bullet, shoots a burst if needed
         {
             isShooting = true;
             if (isBurstShot)
@@ -425,16 +393,14 @@ namespace FPS
             }
             else
             {
-                Instantiate(bullet, shootPosition.position, transform.rotation);
+                createBullet();
             }
 
             yield return new WaitForSeconds(shootRate);
             isShooting = false;
         }
 
-        /**----------------------------------------------------------------
-         * @brief Coroutine to make enemy roam around the nav mesh.
-         */        
+         
         private IEnumerator roam() //enemy chooses a random spot in roamDist and paths to it
         {
             if (!destinationChosen && agent.remainingDistance < 0.05f)
@@ -457,10 +423,8 @@ namespace FPS
             }
         }
 
-        /**----------------------------------------------------------------
-         * @brief
-         */
-        private IEnumerator spottedUIon()
+
+        private IEnumerator spottedUIon() //turns the UI on if the player gets spotted. turns it off after 3seconds
         {
             if (spottingUI.activeInHierarchy)
             {
@@ -474,7 +438,7 @@ namespace FPS
         /**----------------------------------------------------------------
          * @brief 
          */
-        private IEnumerator losingPlayer()
+        private IEnumerator losingPlayer() //starts losing the player if LOS is lost. after a time the player is lost and enemy returns to defautl. Currently set to 5 seconds for testing
         {
             roamDist = 10;
             //make some function for looking around after stopping
@@ -491,20 +455,16 @@ namespace FPS
             percentSpotted = 0;
         }
 
-        /**----------------------------------------------------------------
-         * @brief Coroutine to make the enemy flash red when damaged.
-         */
-        private IEnumerator flashColor()
+ 
+        private IEnumerator flashColor() //make the enemy model flash when taking damage
         {
             model.material.color = Color.red;
             yield return new WaitForSeconds(0.1f);
             model.material.color = Color.white;
         }
         
-        /**----------------------------------------------------------------
-         * @brief Corouting to shoot burst fire at player.
-         */
-        private IEnumerator shootBurst()
+      
+        private IEnumerator shootBurst() //burst that shoots 3 bullets 
         {
             Instantiate(bullet, shootPosition.position, transform.rotation);
             yield return new WaitForSeconds(burstRate);
@@ -513,10 +473,8 @@ namespace FPS
             Instantiate(bullet, shootPosition.position, transform.rotation);
         }
         
-        /**----------------------------------------------------------------
-         * @brief Coroutine to make enemy look around for player.
-         */
-        private IEnumerator lookAround()
+    
+        private IEnumerator lookAround() //WIP: Has the enemy look around a certain degree after reaching patrol spots
         {
             //if spotted look 45 left, 45 right, 180, 45 left 45 right
             //if patrolling rotate player based on index in int list
