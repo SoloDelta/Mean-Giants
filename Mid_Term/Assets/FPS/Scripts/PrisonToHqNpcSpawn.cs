dbe9c2d47e696e5d7695da37e7d3057557c2a463
toddzipp <tdzipp@student.fullsail.edu>
2023-07-04 03:14:43 -0400

    created a spawn to move mission npc from prison to HQ

---
+++ Mid_Term/Assets/FPS/Scripts/PrisonToHqNpcSpawn.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrisonToHqNpcSpawn : MonoBehaviour
+{
+    [SerializeField] GameObject objectToSpawn;
+    [SerializeField] Transform spawnPos;
+    [SerializeField] int numberToSpawn;
+    [SerializeField] float timeBetweenSpawn;
+
+
+        int numberSpawn;
+        bool playerInRange;
+        bool isSpawning;
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (playerInRange && !isSpawning && numberSpawn < numberToSpawn)
+            {
+                StartCoroutine(Spawn());
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                playerInRange = true;
+            }
+        }
+
+        IEnumerator Spawn()
+        {
+            isSpawning = true;
+            Instantiate(objectToSpawn, spawnPos.position, transform.rotation);
+            numberSpawn++;
+            yield return new WaitForSeconds(timeBetweenSpawn);
+            isSpawning = false;
+        }
+}
+
