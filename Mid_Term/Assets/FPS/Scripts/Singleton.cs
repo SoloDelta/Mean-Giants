3c96505fb8a3aed681d1c4d0cdeb9831ce7cd301
DanielDorn <danieldorn7991@gmail.com>
2023-07-04 20:48:12 -0400

    Commit ??: Updated Input to use mouse, keyboard, and gamepad for in game and UI, started to update the UI as it currently has many issues with its anchoring.

---
+++ Mid_Term/Assets/FPS/Scripts/Singleton.cs
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2023 - 2023, The Mean Giants, All Rights Reserved.
+ *
+ * Authors
+ *  - 
+ */
+
+//-----------------------------------------------------------------
+// Using Namespaces
+//-----------------------------------------------------------------
+using System;
+using UnityEngine;
+
+namespace FPS
+{
+    public class Singleton<T>
+    {
+        
+    }
+}
