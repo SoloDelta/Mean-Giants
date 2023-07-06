eccd821cebd5d79163dc6f860b5faff9af1b9801
DanielDorn <danieldorn7991@gmail.com>
2023-07-05 20:13:21 -0400

    Commit ??: added splash screen.

--- Mid_Term/Assets/FPS/Scripts/SplashScreen.cs
+++ Mid_Term/Assets/FPS/Scripts/SplashScreen.cs
@@ -8,11 +8,41 @@
 //-----------------------------------------------------------------
 // Using Namespaces
 //-----------------------------------------------------------------
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace FPS
 {
-    public class SplashScreen
-    {
-        
+    public class SplashScreen : MonoBehaviour
+    {   
+        [SerializeField] private GameObject m_DirectionalLight;
+
+        /**----------------------------------------------------------------
+         * @brief Monobehaviour override.
+         */
+        private void Start()
+        {
+            //-----------------------------------------------------------------
+            // Start transition coroutine.
+            StartCoroutine(TransitionToMainMenuDelayed());
+        }
+
+        /**----------------------------------------------------------------
+         * @brief Monobehaviour override.
+         */
+        private void Update()
+        {
+            this.m_DirectionalLight.GetComponent<Transform>().Rotate(new Vector3(0.0f, -0.1f, 0.0f));
+        }
+
+        /**----------------------------------------------------------------
+         * @brief
+         */
+        IEnumerator TransitionToMainMenuDelayed()
+        {
+            yield return new WaitForSeconds(5);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
